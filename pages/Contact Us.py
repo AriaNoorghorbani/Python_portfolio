bd28288e8f021fb6a69ff6ca9594b00195008774
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-11-15 16:11:36 +0300

    add send email feature

--- pages/Contact Us.py
+++ pages/Contact Us.py
@@ -1,11 +1,20 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="contact-me"):
     email = st.text_input("Your Email:")
-    message = st.text_area(label="Write your message:")
+    raw_message = st.text_area(label="Write your message:")
+    message = f"""\
+Subject: New email form {email}
+
+From: {email}
+{raw_message}
+"""
+
     submit_btn = st.form_submit_button("Submit")
 
     if submit_btn:
-        print("Submited")
+        send_email(message)
+        st.info("Your email was sent successfully.")
