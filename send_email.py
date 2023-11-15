040a68343e8c42a9732ccebff48450ff2653d6cc
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-11-15 16:27:39 +0300

    complete send email feature

--- send_email.py
+++ send_email.py
@@ -1,19 +1,18 @@
 import smtplib, ssl
-
+import os
+from enviremnet import my_env
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
+
     username = "valentino.tarantino@gmail.com"
-    password = "yync kmtp voxf qssj"
+    # password = os.getenv("VALENTINO_SEND_EMAIL_PASSWORD")
+    password = my_env.VALENTINO_SEND_EMAIL_PASSWORD
+
     receiver = "valentino.tarantino@gmail.com"
     context = ssl.create_default_context()
-#     message = """\
-# subject: Answer Email
-# Hi
-# How are you?
-# Bye
-# """
+
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
